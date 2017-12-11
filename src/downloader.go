18d0e4cbe8bf437c155e0160331881d777594af4
olderlong <olderlong@163.com>
2017-12-11 19:59:39 +0800

    download文件拆分为download和parser

--- src/downloader.go
+++ src/downloader.go
@@ -2,23 +2,16 @@ package crawler
 
 //downloader.go 下载页面，获取源代码
 import (
-	"io/ioutil"
 	"net/http"
 )
 
-func GetPageSource(url string) (content string, statusCode int) {
+func GetPage(url string) (resp *http.Response, statusCode int) {
 	resp, err1 := http.Get(url)
 	if err1 != nil {
 		statusCode = -100
 		return
 	}
-	defer resp.Body.Close()
-	data, err2 := ioutil.ReadAll(resp.Body)
-	if err2 != nil {
-		statusCode = -200
-		return
-	}
+
 	statusCode = resp.StatusCode
-	content = string(data)
 	return
 }
--- src/parser.go
+++ src/parser.go
@@ -2,7 +2,7 @@ package crawler
 
 import (
 	"log"
-	"net/url"
+	"net/http"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -11,15 +11,15 @@ import (
 // parser.go 解析页面内容
 
 // LinkParse 根据Url获取网页中当前域名下的URL链接
-func LinkParse(baseURL string) []LinkItem {
+func PageParse(resp *http.Response) []LinkItem {
 
-	doc, err := goquery.NewDocument(baseURL)
+	doc, err := goquery.NewDocumentFromResponse(resp) //这里已经将resp关闭了，对resp的处理要放在前面
 	if err != nil {
 		log.Fatal(err)
 		return nil
 	}
-
-	u, _ := url.Parse(baseURL)
+	// u ,_:=resp.Location()	//location函数从resp的Header中读取Location字段的值，有时会为空，这里不采用
+	u := resp.Request.URL
 	site := u.Scheme + "://" + u.Host
 
 	var items = make([]LinkItem, 16, 32)
