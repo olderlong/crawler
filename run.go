c7ac3d776eed90d93675977fd458394dcfa1e851
olderlong <olderlong@163.com>
2017-12-15 09:13:48 +0800

    parser 添加配置初始化

--- run.go
+++ run.go
@@ -3,6 +3,7 @@ package crawler
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 )
@@ -14,18 +15,24 @@ type Crawler struct {
 func (this *Crawler) Run(args []string) {
 	fmt.Printf("%v", args)
 	fSet := flag.NewFlagSet(args[0], flag.ExitOnError)
-	var ParseConfigFilePath = fSet.String("config", "", "页面解析配置文件完整路径")
+	var ParseConfigFilePath = fSet.String("conf", "", "页面解析配置文件完整路径")
 	fSet.Parse(args[1:])
 
-	ParseConfigs = GetParseConfigs(*ParseConfigFilePath)
+	conf, err := CrawlerConfigureParse(*ParseConfigFilePath)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
 	URLQueue.PushBack(this.StartURL)
 
+	var parser = Parser{ParserConfig: conf, BaseURL: this.StartURL}
 	c := make(chan os.Signal, 0)
 	signal.Notify(c, os.Interrupt, os.Kill)
 
 	IsRunning = true
 	go StartDownload()
-	go StartParse()
+	go parser.Start()
 
 	s := <-c
 	IsRunning = false
